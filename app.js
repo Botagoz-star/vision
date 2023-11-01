255f9a734a7d867daa633b4c35683b01e27290cc
Botagoz-star <58795026+Botagoz-star@users.noreply.github.com>
2023-10-31 22:51:17 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,24 +1,3 @@
-
-const tabs = document.querySelectorAll(".tab");
-const articles = document.querySelectorAll("article");
-for (let i = 0; i < tabs.length; i++) {
-  
-   tabs[i].addEventListener("click", function (event) {
-  const currentTab=event.target;
-  currentTab.classList.add("active");
-   for (let i=0; i<tabs.length; i++){
-    if (tabs[i]!==currentTab){
-        tabs[i].classList.remove("active");
-    }
+function clickFn(){
+    console.log("Hello")
 }
-for(let j=0; j<articles.length; j++){
-    if (currentTab.innerText.toLowerCase().includes(articles[j].id)){
-        articles[j].style.display="block";
-    } else {
-        articles[j].style.display = "none";
-    }
-}
-
-    });
-    }
-   
